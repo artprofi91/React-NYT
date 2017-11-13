52429e9d727d9d9d4934d962e144438247d2d22b
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 15:47:19 -0600

    results

--- client/src/components/Search/Results.js
+++ client/src/components/Search/Results.js
@@ -1,32 +1,22 @@
-// Include React as a dependency
 import React, { Component } from 'react'
-
-// Include our helpers for API calls
 import helpers from "../../utils/helpers";
 
-// Results Component Declaration
 class Results extends Component {
-  // Here we will save states for the contents we save
   state = {
     title: "",
     url: "",
     pubdate: ""
   }
 
-  // This code handles the sending of the search terms to the parent Search component
+  // search term to search Component
   handleClick = (item) => {
-    console.log("CLICKED", item);
-
     helpers.postSaved(item.headline.main, item.pub_date, item.web_url).then(function() {
-      console.log(item.web_url);
+
     });
   }
 
-  // A helper method for mapping through our articles and outputting some HTML
   renderArticles = () => {
     return this.props.results.docs.map((article, index) => {
-
-      // Each article thus reperesents a list group item with a known index
       return (
         <div key={index}>
           <li className="list-group-item">
@@ -38,11 +28,6 @@ class Results extends Component {
                 <a href={article.web_url} rel="noopener noreferrer" target="_blank">
                   <button className="btn btn-default ">View Article</button>
                 </a>
-
-                {/*
-                  By using an arrow function callback to wrap this.handleClick,
-                  we can pass in an article as an argument
-                */}
                 <button className="btn btn-primary" onClick={() => { this.handleClick(article)}}>Save</button>
               </span>
             </h3>
@@ -57,7 +42,7 @@ class Results extends Component {
 
   }
 
-  // A helper method for rendering a container to hold all of our articles
+  // render container to hold articles
   renderContainer = () => {
     return (
       <div className="main-container">
@@ -84,22 +69,18 @@ class Results extends Component {
     );
   }
   render() {
-    // If we have no articles, render this HTML
     if (!this.props.results.docs) {
       return (
         <li className="list-group-item">
           <h3>
             <span>
-              <em>Enter search terms to begin...</em>
+              <em>Enter search terms, please.</em>
             </span>
           </h3>
         </li>
       );
     }
-    // If we have articles, return this.renderContainer() which in turn, returns all the articles
     return this.renderContainer();
   }
 };
-
-// Export the module back to the route
 export default Results;
