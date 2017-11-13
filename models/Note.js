e6f862ddb0e037228eab6d218ceaaa535f1a7309
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 14:04:28 -0600

    models

--- models/Note.js
+++ models/Note.js
@@ -1,29 +1,21 @@
-// Note model
-// ==========
-
-// Require mongoose
 var mongoose = require("mongoose");
-// Create the schema class using mongoose's schema method
+// create schema
 var Schema = mongoose.Schema;
 
-// Create the noteSchema with the schema object
+// create noteSchema with schema object
 var noteSchema = new Schema({
-  // The headline is the article associate with the note
   _headlineId: {
     type: Schema.Types.ObjectId,
     ref: "Headline"
   },
-  // date is just a string
   date: {
     type: Date,
     default: Date.now
   },
-  // as is the noteText
   noteText: String
 });
 
-// Create the Note model using the noteSchema
+// create Note model using noteSchema
 var Note = mongoose.model("Note", noteSchema);
 
-// Export the Note model
 module.exports = Note;
