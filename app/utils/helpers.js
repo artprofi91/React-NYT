9b5483e41605dd5dd5c500cafb51902d947c4031
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 14:55:07 -0600

    helper

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,13 +1,7 @@
-// Include the Axios library for HTTP requests
 var axios = require("axios");
-
-// NYT API Key (Replace with your own API Key)
 var APIKey = "9b3adf57854f4a19b7b5782cdd6e427a";
-
-// Helper Functions
 var helpers = {
 
-  // This will run our query.
   runQuery: function(term, start, end) {
 
     // Adjust to get search terms in proper format
@@ -15,10 +9,7 @@ var helpers = {
     var formattedStart = start.trim() + "0101";
     var formattedEnd = end.trim() + "1231";
 
-
-    console.log("Query Run");
-    // Run a query using Axios. Then return the results as an object with an array.
-    // See the Axios documentation for details on how we structured this with the params.
+    // result as object with array
     return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
       params: {
         "api-key": APIKey,
@@ -32,7 +23,7 @@ var helpers = {
       return results.data.response;
     });
   },
-  // This will return any saved articles from our database
+  // return articles from db
   getSaved: function() {
     return axios.get("/api/saved")
       .then(function(results) {
@@ -40,7 +31,7 @@ var helpers = {
         return results;
       });
   },
-  // This will save new articles to our database
+  // save articles to db
   postSaved: function(title, date, url) {
     var newArticle = { title: title, date: date, url: url };
     return axios.post("/api/saved", newArticle)
@@ -49,7 +40,7 @@ var helpers = {
         return response.data._id;
       });
   },
-  // This will remove saved articles from our database
+  // delete articles from db
   deleteSaved: function(title, data, url) {
     return axios.delete("/api/saved", {
       params: {
@@ -65,6 +56,4 @@ var helpers = {
   }
 };
 
-
-// We export the helpers function
 module.exports = helpers;
