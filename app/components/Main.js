3dfdb7d7bc273293b50a01bd879cb07687b2cae9
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 14:46:22 -0600

    main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,19 +1,12 @@
-// Include React as a dependency
 var React = require("react");
-// Including the Link component from React Router to navigate within our application without full page reloads
-// https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
 var Link = require("react-router").Link;
-
-// Create the Main component
 var Main = React.createClass({
 
   render: function() {
 
     return (
-      // We can only render a single div. So we need to group everything inside of this main-container one
       <div className="main-container">
         <div className="container">
-          {/* Navbar */}
           <nav className="navbar navbar-default" role="navigation">
             <div className="container-fluid">
               <div className="navbar-header">
@@ -33,7 +26,6 @@ var Main = React.createClass({
 
               <div className="collapse navbar-collapse navbar-ex1-collapse">
                 <ul className="nav navbar-nav navbar-right">
-                  {/* Using <Link> in place of <a> and "to" in place of "href" */}
                   <li><Link to="/search">Search</Link></li>
                   <li><Link to="/saved">Saved Articles</Link></li>
                 </ul>
@@ -41,29 +33,27 @@ var Main = React.createClass({
             </div>
           </nav>
 
-          {/* Jumbotron */}
-          <div className="jumbotron">
-            <h2 className="text-center"><strong>(ReactJS) New York Times Article Scrubber</strong></h2>
-            <h3 className="text-center">Search for and save articles of interest.</h3>
+          <div class="jumbotron text-center">
+            <div class="overlay">
+            </div>
+            <div class="background-image">
+            </div>
+            <div class="caption">
+              <h1>Mongo Scraper</h1>
+              <p>New York Times Edition</p>
+            </div>
           </div>
-
-
-          {/* Here we will deploy the sub components (Search or Saved */}
-          {/* These sub-components are getting passed as this.props.children */}
           {this.props.children}
 
-          <footer>
-            <hr />
-            <p className="pull-right">
-              <i className="fa fa-github" aria-hidden="true"></i>
-              Proudly built using React.js
-            </p>
-          </footer>
+          <footer class="footer">
+          <div class="container">
+            <p>New York Times Mongo Scraper</p>
+          </div>
+        </footer>
         </div>
       </div>
     );
   }
 });
 
-// Export the module back to the route
 module.exports = Main;
