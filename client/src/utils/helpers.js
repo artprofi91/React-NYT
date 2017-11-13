ead7d44fe9b830a0cdbf63d6ccdb2c92dcd2f546
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 15:54:54 -0600

    helper

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,24 +1,16 @@
-// Include the Axios library for HTTP requests
 import axios from "axios";
-
-// NYT API Key (Replace with your own API Key)
 var APIKey = "9b3adf57854f4a19b7b5782cdd6e427a";
 
-// Helper Functions
 const helpers = {
 
-  // This will run our query.
+  // run query
   runQuery: function(term, start, end) {
 
-    // Adjust to get search terms in proper format
+    // search terms in proper format
     var formattedTerm = term.trim();
     var formattedStart = start.trim() + "0101";
     var formattedEnd = end.trim() + "1231";
 
-
-    console.log("Query Run");
-    // Run a query using Axios. Then return the results as an object with an array.
-    // See the Axios documentation for details on how we structured this with the params.
     return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
       params: {
         "api-key": APIKey,
@@ -28,29 +20,25 @@ const helpers = {
       }
     })
     .then(function(results) {
-      console.log("Axios Results", results.data.response);
       return results.data.response;
     });
   },
-  // This will return any saved articles from our database
+  // return saved articles from db
   getSaved: function() {
     return axios.get("/api/saved")
       .then(function(results) {
-        console.log("axios results", results);
         return results;
       });
   },
-  // This will save new articles to our database
+  // save new articles to db
   postSaved: function(title, date, url) {
     var newArticle = { title: title, date: date, url: url };
-    console.log('postSaved', title)
     return axios.post("/api/saved", newArticle)
       .then(function(response) {
-        console.log("axios results", response.data._id);
         return response.data._id;
       });
   },
-  // This will remove saved articles from our database
+  // remove article from db
   deleteSaved: function(title, data, url) {
     return axios.delete("/api/saved", {
       params: {
@@ -60,12 +48,9 @@ const helpers = {
       }
     })
     .then(function(results) {
-      console.log("axios results", results);
       return results;
     });
   }
 };
 
-
-// We export the helpers function
 export default helpers;
