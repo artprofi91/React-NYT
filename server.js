646f9b2ce1e5313eb7b2126d1eacdc11308c6659
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-11-13 14:38:16 -0600

    query.js

--- server.js
+++ server.js
@@ -1,48 +1,96 @@
-// Web Scraper Homework Solution Example
-// (be sure to watch the video to see
-// how to operate the site in the browser)
-// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
-
-// Require our dependencies
+// Include Server Dependencies
 var express = require("express");
-var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-// Set up our port to be either the host's designated port, or 3000
-var PORT = process.env.PORT || 3000;
+// Require Schemas
+var Article = require("./server/model");
 
-// Instantiate our Express App
+// Create Instance of Express
 var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
 
-// Require our routes
-var routes = require("./routes");
-
-// Designate our public folder as a static directory
-app.use(express.static("public"));
-
-// Connect Handlebars to our Express app
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Use bodyParser in our app
-app.use(bodyParser.urlencoded({ extended: false }));
+// Run Morgan for Logging
+app.use(logger("dev"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Have every request go through our route middleware
-app.use(routes);
+app.use(express.static("./public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// -------------------------------------------------
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+// MongoDB Configuration configuration
+mongoose.connect("mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
 });
 
-// Listen on the port
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+// -------------------------------------------------
+
+// Route to get all saved articles
+app.get("/api/saved", function(req, res) {
+
+  Article.find({})
+    .exec(function(err, doc) {
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    });
+});
+
+// Route to add an article to saved list
+app.post("/api/saved", function(req, res) {
+  var newArticle = new Article(req.body);
+
+  console.log(req.body);
+
+  newArticle.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+// Route to delete an article from saved list
+app.delete("/api/saved/", function(req, res) {
+
+  var url = req.param("url");
+
+  Article.find({ url: url }).remove().exec(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send("Deleted");
+    }
+  });
+});
+
+// Any non API GET routes will be directed to our React App and handled by React Router
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+// -------------------------------------------------
+
 app.listen(PORT, function() {
-  console.log("Listening on port: " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
